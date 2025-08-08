1178f7c36bc95a7d69a7efb5c370ac23cc92488f
MSOLO10-creator <m.sololtda@gmail.com>
2025-08-08 02:50:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const app = document.getElementById("app");
+    app.textContent = "Dashboard financeiro conectado à sua planilha do Google!";
+});
